9fb48ed6e632cfebaf60b28c39484a1fdd244a1e
Filype <lyperodrigues03@gmail.com>
2025-01-05 18:47:59 -0300

    fix nullable return on domain repository

--- Paschoalotto.Desafio.Domain/Repositories/IRepository.cs
+++ Paschoalotto.Desafio.Domain/Repositories/IRepository.cs
@@ -5,6 +5,6 @@ namespace Paschoalotto.Desafio.Domain.Repositories;
 public interface IRepository<T> where T : Entity
 {
     Task<IEnumerable<T>> GetAllAsync();
-    Task<T> GetByIdAsync(string id);
-    Task<T> UpdateAsync(T entity);
+    Task<T?> GetByIdAsync(string id);
+    Task<T?> UpdateAsync(T entity);
 }
