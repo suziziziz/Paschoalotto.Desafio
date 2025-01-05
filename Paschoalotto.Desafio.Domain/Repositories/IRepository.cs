6b3d9174215ad71999b915011fabbf57ae0d39b3
Filype <lyperodrigues03@gmail.com>
2025-01-05 18:47:10 -0300

    fix update id param on domain repository interface

--- Paschoalotto.Desafio.Domain/Repositories/IRepository.cs
+++ Paschoalotto.Desafio.Domain/Repositories/IRepository.cs
@@ -5,6 +5,6 @@ namespace Paschoalotto.Desafio.Domain.Repositories;
 public interface IRepository<T> where T : Entity
 {
     Task<IEnumerable<T>> GetAllAsync();
-    Task<T> GetByIdAsync();
+    Task<T> GetByIdAsync(string id);
     Task<T> UpdateAsync(T entity);
 }
